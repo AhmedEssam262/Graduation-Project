7de371ae1f486e15bb3e6d430a7db96b8a2715f6
Ahmed Magdy <85253814+Ahmed5920@users.noreply.github.com>
2023-03-20 19:33:33 +0200

    Update feedbackController.php

--- app/Http/Controllers/feedbackController.php
+++ app/Http/Controllers/feedbackController.php
@@ -10,16 +10,78 @@ use Illuminate\Http\Request;
 
 class feedbackController extends Controller
 {
-    public function filter_feedback($username)
+    public function filter_feedback()
     {
-        $userdoc = User::where('username', $username)->first();
+        $username=null;
+        $limit=0;
+        if(isset($_GET['username'])){
+            $username=$_GET['username'];
 
-        $doctor = Doctor::where('username', $username)->first();
 
-        $feedback = feedback::where('feedback_to', $doctor->id)->get();
+
+            $doctor = Doctor::where('username', $username)->first();
+            $doctor_user = User::where('username', $username)->first();
+            $feedback = feedback::where('feedback_to', $doctor->user_id)->get()->limit(5);
+            $res = array();
+            foreach ($feedback as $f) {
+                $userpat = User::where('id', $f->feedback_from)->first();
+                $userData = [
+                    'state' => 'good, ok',
+                    'message' => 'information retreived successfully',
+                    'data' => [
+                        'user' => [
+                            'feedback_from' => $f->feedback_from,
+                            'feedback_to' => $f->feedback_to,
+                            'rate' => $f->rate,
+                            'issued_time' => $f->updated_at,
+                            'feedback' => $f->feedback,
+                            'uimgUrl' => $userpat->img_url,
+                            'dimgUrl' => $doctor_user->img_url,
+                            'username' => $userpat->username,
+                            'doctorName' => $doctor->name,
+                        ]
+                    ]
+                ];
+                array_push($res, $userData);
+
+            }
+            return response(compact('res'), 200);
+        }
+        if(isset($_GET['limit'])){
+            $limit=$_GET['limit'];
+            $feedback = feedback::all()->get()->limit($limit);
+            $res = array();
+            foreach ($feedback as $f) {
+                $userpat = User::where('id', $f->feedback_from)->first();
+                $userpdoc = User::where('id', $f->feedback_to)->first();
+                $userData = [
+                    'state' => 'good, ok',
+                    'message' => 'information retreived successfully',
+                    'data' => [
+                        'user' => [
+                            'feedback_from' => $f->feedback_from,
+                            'feedback_to' => $f->feedback_to,
+                            'rate' => $f->rate,
+                            'issued_time' => $f->updated_at,
+                            'feedback' => $f->feedback,
+                            'uimgUrl' => $userpat->img_url,
+                            'dimgUrl' => $userpdoc->img_url,
+                            'username' => $userpat->username,
+                            'doctorName' => $userpdoc->name,
+                        ]
+                    ]
+                ];
+                array_push($res, $userData);
+
+            }
+            return response(compact('res'), 200);
+        }
+
+        $feedback = feedback::all()->get()->limit(5);
         $res = array();
         foreach ($feedback as $f) {
             $userpat = User::where('id', $f->feedback_from)->first();
+            $userpdoc = User::where('id', $f->feedback_to)->first();
             $userData = [
                 'state' => 'good, ok',
                 'message' => 'information retreived successfully',
@@ -31,15 +93,15 @@ class feedbackController extends Controller
                         'issued_time' => $f->updated_at,
                         'feedback' => $f->feedback,
                         'uimgUrl' => $userpat->img_url,
-                        'dimgUrl' => $userdoc->img_url,
+                        'dimgUrl' => $userpdoc->img_url,
                         'username' => $userpat->username,
-                        'doctorName' => $doctor->name,
+                        'doctorName' => $userpdoc->name,
                     ]
                 ]
             ];
             array_push($res, $userData);
-
         }
         return response(compact('res'), 200);
+
     }
 }
