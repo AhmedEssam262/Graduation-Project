da88c8fcebab772f7da082a0801d18bea84c76cc
Ahmed Essam <ahmed.essameldin262@gmail.com>
2023-04-14 20:10:24 +0200

    updating database

---
+++ app/Models/Reply.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reply extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'post_id',
+        'reply_id',
+        'reply_on',
+        'num_replies',
+        'like_emoji',
+        'dislike',
+        'angry',
+
+    ];
+}
